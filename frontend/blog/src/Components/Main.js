20830c50d9eaaccdd11a0d2544a29a05b5fedf5c
Unknown <kjhop@wp.pl>
2018-08-06 18:16:56 +0200

    Started working at AddPost component

--- frontend/blog/src/Components/Main.js
+++ frontend/blog/src/Components/Main.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import '../Styles/Main.css';
+import Post from './Post';
 
 class Main extends React.Component{
     render(){
         return(
             <main>
-                <h1>This week:</h1>
                 <div className='thisWeekPosts'>
-                    
+                    <Post title='Kozak post' tags='post fajny taki' text='Lorem ipsum get text from database just for now this is dummy text to check how it looks'/>
                 </div>
             </main>
         )
