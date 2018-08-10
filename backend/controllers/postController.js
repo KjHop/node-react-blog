284b11d526d60455aa8dbb5c39def095b1bcf56d
Unknown <kjhop@wp.pl>
2018-08-10 22:46:16 +0200

    Adding Posts to database

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -5,21 +5,29 @@ mongoose.connect('mongodb://test:testtest1@ds113442.mlab.com:13442/node-blog')
 
 const postSchema = new mongoose.Schema({
     title: String,
-    date: Date,
+    date: String,
     tags: String,
     text: String,
     src: String,
-    postUrl: String
+    postNumber: Number
 })
 const Post = mongoose.model('Post', postSchema);
 const urlencodedParser = bodyParser.urlencoded({extended:false});
 
 module.exports = app =>{
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
     app.get('/', (request, response)=>{
         Post.find({}, (err, data)=>{
             if (err) throw err;
             response.send(JSON.stringify({posts: data}));
         });
+    });
+    app.post('/add-post', urlencodedParser, (request, response)=>{
+        let newPost = Post(request.body).save((err, data)=>{
+            if (err) throw err;
+            response.json(data);
+        })
     });
 
 }
