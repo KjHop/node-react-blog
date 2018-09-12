9a9db49afacc5bf46c36e407ab6e8605a6260641
Unknown <kjhop@wp.pl>
2018-09-12 20:39:08 +0200

    Getting table of contents from server

--- frontend/blog/src/Components/AllPosts.js
+++ frontend/blog/src/Components/AllPosts.js
@@ -2,17 +2,40 @@ import * as React from 'react';
 import '../Styles/AllPosts.css';
 
 //Get from database
-let allPosts = ['Gitarka', 'Siemka', 'Jan', 'Blogowanie', 'Funny', 'Moments', 'Godot']
-const listOfPosts = allPosts.map((title)=><li key={title}>{title}</li>);
+// let allPosts = ['Gitarka', 'Siemka', 'Jan', 'Blogowanie', 'Funny', 'Moments', 'Godot']
+// const listOfPosts = allPosts.map((title)=><li key={title}>{title}</li>);
 
 
 class AllPosts extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            allPosts:[]
+        }
+    }
+    componentWillMount(){
+        fetch('http://192.168.8.100:8080/')
+        .then(response=>{
+            return response.json();
+        })
+        .then(myJson=>{
+            let num = 1;
+            let allPosts = [];
+            myJson.posts.forEach(post=>{
+                allPosts.push(<li key={num}><a href={post.src}>{num}.{post.title}</a></li>);
+                num++;
+            });
+            this.setState({
+                allPosts: allPosts
+            });
+        });;
+    }
     render(){
         return(
             <div className='AllPosts'>
-                <h1>Content list:</h1>
+                <h1>Table of Contents:</h1>
                 <ul>
-                    {listOfPosts}
+                    {this.state.allPosts}
                 </ul>
             </div>
         )
